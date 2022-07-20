25b5ffd9366d214a7d4de0dbb854ee9e8e28021e
akshat_jain2 <akshat_jain2@NGL014618.persistent.co.in>
2022-07-20 14:54:58 +0530

    Some changes

--- src/com/project/dl/dao/DAOConnection.java
+++ src/com/project/dl/dao/DAOConnection.java
@@ -8,14 +8,11 @@ public class DAOConnection {
 			
 			
 			String mysqlJDBCDriver="oracle.jdbc.driver.OracleDriver"; 
-			//String url="jdbc:mysql://localhost:3306/core_banking?autoReconnect=true&useSSL=false"; 
-		//Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysys?autoReconnect=true&useSSL=false","root","pass")
 			String url="jdbc:oracle:thin:@localhost:1521:xe";
 			String user = "core_banking";	
 			String pass = "Akshat1999**"; 
 			Class.forName(mysqlJDBCDriver);
 			con =DriverManager.getConnection(url,user,pass);
-			System.out.println("Connection established");
 			
 		}
 		catch (Exception e) {
